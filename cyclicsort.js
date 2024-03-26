938df70b6916bbaaa9bf1c414541cb1f2e4eef65
MeheretAl <meheretxo4@gmail.com>
2024-03-26 15:52:41 +0300

    added cyclicsort.js

---
+++ cyclicsort.js
@@ -0,0 +1,19 @@
+function cycleSort(arr) {
+    var i = 0
+    while (i < arr.length) {
+        var correctIndex = arr[i] - 1
+        if (0 <= correctIndex && arr[i] != arr[correctIndex]) {
+            var swap = arr[i]
+            arr[i] = arr[correctIndex]
+            arr[correctIndex] = swap
+        } else {
+            i += 1;
+        }
+    }
+
+    return arr
+
+}
+
+arr = [7, 8, 9, 11, 12]
+console.log(cycleSort(arr))
