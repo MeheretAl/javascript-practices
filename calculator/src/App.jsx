4e466a9d163424f0e868b839325a84944a54a2d4
MeheretAl <meheretxo4@gmail.com>
2024-04-02 02:16:03 +0300

    added handleButtonClick on Numbers and operands

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -21,28 +21,28 @@ function App() {
               <input type="text" value={value} />
             </div>
             <div className='Row'>
-              <button className='Op' value='7'>7</button>
-              <button className='Op' value='8'>8</button>
-              <button className='Op' value='9'>9</button>
-              <button className='Op' value='/'>/</button>
+              <button className='Op' value='7' onClick={handleButtonClick}>7</button>
+              <button className='Op' value='8' onClick={handleButtonClick}>8</button>
+              <button className='Op' value='9' onClick={handleButtonClick}>9</button>
+              <button className='Op' value='/' onClick={handleButtonClick}>/</button>
             </div>
             <div className='Row'>
-              <button className='Op' value="4">4</button>
-              <button className='Op' value="5">5</button>
-              <button className='Op' value="6">6</button>
-              <button className='Op' value="*">*</button>
+              <button className='Op' value="4" onClick={handleButtonClick}>4</button>
+              <button className='Op' value="5" onClick={handleButtonClick}>5</button>
+              <button className='Op' value="6" onClick={handleButtonClick}>6</button>
+              <button className='Op' value="*" onClick={handleButtonClick}>*</button>
             </div>
             <div className='Row' >
-              <button className='Op' value='3'>3</button>
-              <button className='Op' value='2'>2</button>
-              <button className='Op' value='1'>1</button>
-              <button className='Op' value='-'>-</button>
+              <button className='Op' value='3' onClick={handleButtonClick}>3</button>
+              <button className='Op' value='2' onClick={handleButtonClick}>2</button>
+              <button className='Op' value='1' onClick={handleButtonClick}>1</button>
+              <button className='Op' value='-' onClick={handleButtonClick}>-</button>
             </div>
             <div className='Row'>
-              <button className='Op' value='C'>C</button>
-              <button className='Op' value='0'>0</button>
-              <button className='Op' value='&larr;'>&larr;</button>
-              <button className='Op' value='='>=</button>
+              <button className='Op' value='C' >C</button>
+              <button className='Op' value='0' onClick={handleButtonClick}>0</button>
+              <button className='Op' value='&larr;' >&larr;</button>
+              <button className='Op' value='=' >=</button>
             </div>
           </form>
         </div>
