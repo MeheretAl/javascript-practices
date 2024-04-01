d61fb75c48eb3ef7f80054b5b9b36d66e3017d61
MeheretAl <meheretxo4@gmail.com>
2024-04-02 02:09:50 +0300

    added handleButtonClick and values for each button

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -3,6 +3,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [value, setValue] = useState(' ');
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+
+    setValue(value + e.target.value);
+  };
+
 
   return (
     <>
@@ -10,31 +18,31 @@ function App() {
         <div className="calculator">
           <form action="">
             <div className='Row'>
-              <input type="text" name="" id="" />
+              <input type="text" value={value} />
             </div>
             <div className='Row'>
-              <button className='Op'>7</button>
-              <button className='Op'>8</button>
-              <button className='Op'>9</button>
-              <button className='Op'>/</button>
+              <button className='Op' value='7'>7</button>
+              <button className='Op' value='8'>8</button>
+              <button className='Op' value='9'>9</button>
+              <button className='Op' value='/'>/</button>
             </div>
             <div className='Row'>
-              <button className='Op'>4</button>
-              <button className='Op'>5</button>
-              <button className='Op'>6</button>
-              <button className='Op'>*</button>
+              <button className='Op' value="4">4</button>
+              <button className='Op' value="5">5</button>
+              <button className='Op' value="6">6</button>
+              <button className='Op' value="*">*</button>
             </div>
             <div className='Row' >
-              <button className='Op'>3</button>
-              <button className='Op'>2</button>
-              <button className='Op'>1</button>
-              <button className='Op'>-</button>
+              <button className='Op' value='3'>3</button>
+              <button className='Op' value='2'>2</button>
+              <button className='Op' value='1'>1</button>
+              <button className='Op' value='-'>-</button>
             </div>
             <div className='Row'>
-              <button className='Op'>C</button>
-              <button className='Op'>0</button>
-              <button className='Op'>&larr;</button>
-              <button className='Op'>=</button>
+              <button className='Op' value='C'>C</button>
+              <button className='Op' value='0'>0</button>
+              <button className='Op' value='&larr;'>&larr;</button>
+              <button className='Op' value='='>=</button>
             </div>
           </form>
         </div>
