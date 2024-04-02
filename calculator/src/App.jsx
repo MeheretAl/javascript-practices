886620bc3c94cd338a77a44825502c80c9dba70a
MeheretAl <meheretxo4@gmail.com>
2024-04-02 16:21:27 +0300

    removed unnecessary classNames

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -6,8 +6,6 @@ function App() {
   const [value, setValue] = useState(' ');
 
   const handleButtonClick = (e) => {
-
-
     setValue(value + e.target.value);
   };
 
@@ -21,27 +19,26 @@ function App() {
               <input type="text" value={value} />
             </div>
             <div className='Row'>
-              <button className='Op' value='7' onClick={handleButtonClick}>7</button>
-              <button className='Op' value='8' onClick={handleButtonClick}>8</button>
-              <button className='Op' value='9' onClick={handleButtonClick}>9</button>
-              <button className='Op' value='/' onClick={handleButtonClick}>/</button>
+              <button value='7' onClick={handleButtonClick}>7</button>
+              <button value='8' onClick={handleButtonClick}>8</button>
+              <button value='9' onClick={handleButtonClick}>9</button>
+              <button value='/' onClick={handleButtonClick}>/</button>
             </div>
             <div className='Row'>
-              <button className='Op' value="4" onClick={handleButtonClick}>4</button>
-              <button className='Op' value="5" onClick={handleButtonClick}>5</button>
-              <button className='Op' value="6" onClick={handleButtonClick}>6</button>
-              <button className='Op' value="*" onClick={handleButtonClick}>*</button>
+              <button value="4" onClick={handleButtonClick}>4</button>
+              <button value="5" onClick={handleButtonClick}>5</button>
+              <button value="6" onClick={handleButtonClick}>6</button>
+              <button value="*" onClick={handleButtonClick}>*</button>
             </div>
             <div className='Row' >
-              <button className='Op' value='3' onClick={handleButtonClick}>3</button>
-              <button className='Op' value='2' onClick={handleButtonClick}>2</button>
-              <button className='Op' value='1' onClick={handleButtonClick}>1</button>
-              <button className='Op' value='-' onClick={handleButtonClick}>-</button>
+              <button value='3' onClick={handleButtonClick}>3</button>
+              <button value='2' onClick={handleButtonClick}>2</button>
+              <button value='1' onClick={handleButtonClick}>1</button>
+              <button value='-' onClick={handleButtonClick}>-</button>
             </div>
             <div className='Row'>
-              <button className='Op' value='C' >C</button>
-              <button className='Op' value='0' onClick={handleButtonClick}>0</button>
-
+              <button onClick={e => setValue(" ")}>C</button>
+              <button value='0' onClick={handleButtonClick}>0</button>
               <button className='equal' value='=' onClick={e => setValue(eval(value))} >=</button>
             </div>
           </form>
