52608622adf2da433c54e2c9eba58b181fc312dd
MeheretAl <meheretxo4@gmail.com>
2024-04-04 16:41:21 +0300

    doing the calculations using the eval function

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,51 +1,60 @@
-import { useState } from 'react'
-
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [value, setValue] = useState(' ');
+  const [value, setValue] = useState('');
 
-  const handleButtonClick = (e) => {
-    setValue(value + e.target.value);
-  };
+  function handleClear() {
+    setValue('')
+  }
 
+  function handleDelete() {
+    setValue(value.slice(0, -1))
+  }
+
+  function handleClick(event) {
+    setValue(value + event.target.value)
+  }
+
+  function handleOperations(event) {
+    setValue(eval(value))
+  }
 
   return (
-    <>
-      <div className="container">
-        <div className="calculator">
-          <form action="" onSubmit={e => e.preventDefault()}>
-            <div className='Row'>
-              <input type="text" value={value} />
-            </div>
-            <div className='Row'>
-              <button value='7' onClick={handleButtonClick}>7</button>
-              <button value='8' onClick={handleButtonClick}>8</button>
-              <button value='9' onClick={handleButtonClick}>9</button>
-              <button value='/' onClick={handleButtonClick}>/</button>
-            </div>
-            <div className='Row'>
-              <button value="4" onClick={handleButtonClick}>4</button>
-              <button value="5" onClick={handleButtonClick}>5</button>
-              <button value="6" onClick={handleButtonClick}>6</button>
-              <button value="*" onClick={handleButtonClick}>*</button>
-            </div>
-            <div className='Row' >
-              <button value='3' onClick={handleButtonClick}>3</button>
-              <button value='2' onClick={handleButtonClick}>2</button>
-              <button value='1' onClick={handleButtonClick}>1</button>
-              <button value='-' onClick={handleButtonClick}>-</button>
-            </div>
-            <div className='Row'>
-              <button onClick={e => setValue(" ")}>C</button>
-              <button value='0' onClick={handleButtonClick}>0</button>
-              <button className='equal' value='=' onClick={e => setValue(eval(value))} >=</button>
-            </div>
-          </form>
+    <div className="container">
+      <div className="calculator">
+        <form>
+          <input type="text" value={value} readOnly />
+        </form>
+
+        <div className="keys">
+          <button className='clear' onClick={handleClear}>C</button>
+          <button onClick={handleDelete}>&larr;</button>
+          <button value={'/'} onClick={handleClick}>&divide;</button>
+
+          <button value={7} onClick={handleClick}>7</button>
+          <button value={8} onClick={handleClick}>8</button>
+          <button value={9} onClick={handleClick}>9</button>
+          <button value={'*'} onClick={handleClick}>*</button>
+
+          <button value={4} onClick={handleClick}>4</button>
+          <button value={5} onClick={handleClick}>5</button>
+          <button value={6} onClick={handleClick}>6</button>
+          <button value={'-'} onClick={handleClick}>-</button>
+
+          <button value={1} onClick={handleClick}>1</button>
+          <button value={2} onClick={handleClick}>2</button>
+          <button value={3} onClick={handleClick}>3</button>
+          <button value={'+'} onClick={handleClick}>+</button>
+
+          <button className="zero" value={0} onClick={handleClick}>0</button>
+          <button value={'.'} onClick={handleClick}>.</button>
+          <button className='equals' value={'='} onClick={handleOperations}>=</button>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
